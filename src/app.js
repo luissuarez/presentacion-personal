28763642f07e9a0fb7ab877fcb8b0ce25373021d
adrianpx2000.suarez@gmail.com <adrianpx2000.suarez@gmail.com>
2023-03-20 20:23:41 -0300

    Api Rest.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("hola");
+const express = require('express');
+const req = require('express/lib/request');
+const app = express();
+const PORT = 3001;
+
+app.get('/api', (req, res) => {
+    res.json({mensaje : 'hola mundo'}).status(200);
+});
+
+app.listen(PORT, () => { 
+    console.log('Estoy escuchando en el puerto ' + PORT);
+});
